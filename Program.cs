8bd88f8712faa1623415fe8ba1f2206631620e60
KyleBax <102880750+KyleBax@users.noreply.github.com>
2022-10-12 21:03:49 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the gallows!");
+            Console.WriteLine("It's up to you to save these poor souls!");
+            Console.WriteLine("Can you do it before they all die?");
+            Console.WriteLine("It's time to Hangman!!!!!");
+
+            List<string> wordList = new List<string>();
+            wordList.Add("analysis");
+            wordList.Add("blackmail");
+            wordList.Add("kitchen");
+            wordList.Add("cast");
+            wordList.Add("superintendent");
+            wordList.Add("legislation");
+            wordList.Add("problem");
+            wordList.Add("bare");
+            wordList.Add("take");
+            wordList.Add("cycle");
+
+            bool alive = true;
+
+            Random random = new Random();
+            int index = random.Next(wordList.Count);
+            string word = wordList[index];
+            Console.WriteLine(word);
+
+            while (alive == true)
+            {
+                string guess = Console.ReadLine().ToLower();
+                if (word.Contains(guess)){
+                    Console.WriteLine("Well done");
+                }
+                if (guess == word)
+                {
+                    Console.WriteLine("Congrats");
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
