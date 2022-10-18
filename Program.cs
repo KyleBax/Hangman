def58ad275a56da06fbb38e72bb424c065652577
KyleBax <102880750+KyleBax@users.noreply.github.com>
2022-10-18 23:07:08 +1300

    changed where the game writes a few lines as they were being cleared before being read, added in a game counter made the difficulty increase with each game. prevented winning by entering 1 successful letter continously

--- Program.cs
+++ Program.cs
@@ -6,10 +6,7 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the gallows!");
-            Console.WriteLine("It's up to you to save these poor souls!");
-            Console.WriteLine("Can you do it before they all die?");
-            Console.WriteLine("It's time to Hangman!!!!!");
+
 
             List<string> wordList = new List<string>();
             wordList.Add("analysis");
@@ -23,11 +20,18 @@ namespace Hangman
             wordList.Add("take");
             wordList.Add("cycle");
 
+            int games = 1;
+
             string keepPlaying = "y";
 
             while (keepPlaying == "y")
             {
                 Console.Clear();
+
+                Console.WriteLine("Welcome to the gallows!");
+                Console.WriteLine("It's up to you to save these poor souls!");
+                Console.WriteLine("Can you do it before they all die?");
+                Console.WriteLine("It's time to Hangman!!!!!");
 
                 List<string> incorrectGuesses = new List<string>();
 
@@ -39,19 +43,25 @@ namespace Hangman
                 int index = random.Next(wordList.Count);
                 string word = wordList[index];
 
-                //to be removed after everything else is working
-                Console.WriteLine(word);
-
-                //may not be needed, added as I was thinking of how to display the word length
-                int length = word.Length;
-                Console.WriteLine(length);
 
 
+
+                Console.WriteLine("Game " + games);
 
 
                 while (inSession)
                 {
 
+                    Console.WriteLine("Your guesses so far");
+                    foreach (string guess in incorrectGuesses)
+                    {
+                        Console.Write(guess + " ");
+
+                    }
+                    Console.WriteLine(" ");
+
+
+                    int success = word.Length;
                     for (int i = 1; i <= word.Length; i++)
                     {
                         char character = word[i - 1];
@@ -59,6 +69,9 @@ namespace Hangman
                         if (correctGuesses.Contains(letter))
                         {
                             Console.Write(letter + " ");
+
+                            success--;
+
                         }
                         else
                         {
@@ -82,23 +95,17 @@ namespace Hangman
 
                     }
 
-                    //TODO make so wins when all letters in word are guessed
-                    if (correctGuesses.Count == word.Length)
+                    if (success == 1)
                     {
-                        Console.WriteLine("Congrats");
+                        Console.WriteLine("Congrats, you win");
+                        Console.WriteLine("The word was " + word);
+                        games++;
                         inSession = false;
                     }
 
-                    Console.WriteLine("Your guesses so far");
-                    foreach (string guess in incorrectGuesses)
-                    {
-                        Console.Write(guess + " ");
-
-                    }
-                    Console.WriteLine(" ");
 
                     // want to increase dificulty by number of play throughs by reducing number of guesses available after each succesful game reseting on a failed game
-                    if (incorrectGuesses.Count >= 10)
+                    if (incorrectGuesses.Count >= 10 - games)
                     {
                         Console.WriteLine("You have been hung!!!");
                         Console.WriteLine("The word was " + word);
